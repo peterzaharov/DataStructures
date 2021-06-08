ef66f7df44f201e160e68025578bd1efbd14df33
Пётр Захаров <peterzaharov@outlook.com>
2021-06-08 08:09:25 +0500

    Добавьте файлы проекта.

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LinkedList.Model
+{
+    /// <summary>
+    /// Ячейка списка.
+    /// </summary>
+    public class Item<T>
+    {
+        private T data = default(T);
+
+        /// <summary>
+        /// Данные хранимые в ячейке списка.
+        /// </summary>
+        public T Data
+        {
+            get { return data; }
+            set
+            {
+                if (value != null)
+                    data = value;
+                else
+                    throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        /// <summary>
+        /// Следующая ячейка списка.
+        /// </summary>
+        public Item<T> Next { get; set; }
+        public Item(T data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Model.LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Delete(3);
+            list.Delete(1);
+            list.Delete(7);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.AppendHead(7);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.InsertAfter(4, 8);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
