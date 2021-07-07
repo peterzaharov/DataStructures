1e1bfe3f1eda794e2a88f8ea92c063e0a7e4c1fc
Пётр Захаров <peterzaharov@outlook.com>
2021-07-07 22:47:35 +0500

    Добавлен граф (graph)

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LinkedList.Model.BinarySearchTree;
+using LinkedList.Model.Graph;
 using LinkedList.Model.HashTable;
 using LinkedList.Model.Heap;
 using LinkedList.Model.Map;
@@ -12,6 +13,46 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+            {
+                var graph = new Graph();
+
+                var v1 = new Vertex(1);
+                var v2 = new Vertex(2);
+                var v3 = new Vertex(3);
+                var v4 = new Vertex(4);
+                var v5 = new Vertex(5);
+                var v6 = new Vertex(6);
+                var v7 = new Vertex(7);
+
+                graph.AddVertex(v1);
+                graph.AddVertex(v2);
+                graph.AddVertex(v3);
+                graph.AddVertex(v4);
+                graph.AddVertex(v5);
+                graph.AddVertex(v6);
+                graph.AddVertex(v7);
+
+                graph.AddEdges(v1, v2);
+                graph.AddEdges(v1, v3);
+                graph.AddEdges(v3, v4);
+                graph.AddEdges(v2, v5);
+                graph.AddEdges(v2, v6);
+                graph.AddEdges(v5, v6);
+                graph.AddEdges(v6, v5);
+
+                var matrix = graph.GetMatrix();
+                for (int i = 0; i < graph.VertexCount; i++)
+                {
+                    for (int j = 0; j < graph.VertexCount; j++)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
             {
                 var rnd = new Random();
                 var startItems = new List<int>();
